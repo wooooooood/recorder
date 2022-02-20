e0bbadaf9c4d03a103303250cac1e4f1f557919f
wooooooood <13pft13@gmail.com>
2022-02-20 05:59:34 -0800

    feat. show spent time

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Input from "./components/Input";
 import Item, { ItemProps } from "./components/Item";
@@ -6,15 +6,25 @@ import Item, { ItemProps } from "./components/Item";
 function App() {
   const [list, setList] = useState<Array<ItemProps>>([]);
 
+  useEffect(() => {
+    setList([
+      ...list.map((x, idx) => {
+        if (idx === list.length - 1) return x;
+        return { ...x, newTime: list[idx + 1].time };
+      }),
+    ]);
+  }, [list.length]);
+
   const Enter = (content: string) => {
-    setList([...list, { time: new Date(), content }]);
+    const now = new Date();
+    setList([...list, { time: now, content }]);
   };
 
   return (
     <div className="App">
       <Input onInput={Enter} />
       {(list || []).map((x, key) => (
-        <Item key={key} time={x.time} content={x.content} />
+        <Item key={key} time={x.time} content={x.content} newTime={x.newTime} />
       ))}
     </div>
   );
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -3,13 +3,17 @@ import moment from "moment";
 export interface ItemProps {
   time: Date;
   content: string;
+  newTime?: Date;
 }
 
-const Item = ({ time, content }: ItemProps) => {
+const Item = ({ time, content, newTime }: ItemProps) => {
   return (
     <div className="flex items-center space-x-2">
       <p className="text-gray-400">{moment(time).format("HH:mm:ss")}</p>
       <p>{content}</p>
+      <p className="text-red-400">
+        {newTime && `(${moment(newTime).diff(moment(time), "minute")}min)`}
+      </p>
     </div>
   );
 };
