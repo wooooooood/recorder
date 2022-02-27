89bd21c28f484ef71d5da0743e52a4884789367f
wooooooood <13pft13@gmail.com>
2022-02-27 17:44:56 +0900

    style. change item ui and use monospace font

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -8,11 +8,15 @@ export interface ItemProps {
 
 const Item = ({ time, content, newTime }: ItemProps) => {
   return (
-    <div className="flex items-center space-x-2">
-      <p className="text-gray-400">{moment(time).format("HH:mm:ss")}</p>
-      <p>{content}</p>
+    <div className="flex items-center justify-between">
+      <div className="flex items-center space-x-2">
+        <p className="text-gray-400 w-20 font-['Ubuntu_Mono']">
+          {moment(time).format("HH:mm:ss")}
+        </p>
+        <p>{content}</p>
+      </div>
       <p className="text-red-400">
-        {newTime && `(${moment(newTime).diff(moment(time), "minute")}min)`}
+        {newTime && `${moment(newTime).diff(moment(time), "minute")}min`}
       </p>
     </div>
   );
